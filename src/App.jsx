d80b761a394811ac824a2f524a25943c3846013b
Deekshitha-m <deekshitham12052001@gmail.com>
2024-03-29 00:12:30 +0530

    text red-blue

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <h1 className='text-red-500 text-4xl font-bold'>App</h1>
+    <h1 className='text-blue-500 text-4xl font-bold'>App</h1>
         
     </>
   )
